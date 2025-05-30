697ac9adc73fff9a5b78ee8d50b8b2c4f61c9bb6
raquelpz <rpz.ra.chel@gmail.com>
2025-05-30 21:12:16 +0200

    Movimiento del personaje y funcionalidad de los temas

---
+++ script.js
@@ -0,0 +1,76 @@
+// DATOS
+const mainAll = document.querySelector ("main");
+const elementPJ = document. querySelector("person");
+const sun = document. querySelector(".day");
+const moon = document. querySelector(".night");
+const bodyAll = document.querySelector("body");
+const theme = document.querySelector(".theme");
+let movePJ = 0;
+let skyPJ = 0;
+let imgPJ = "avatar-prueba.webp";
+let perfilPJ = true;
+let jumpPJ = false;
+let personaje;
+
+//INCIO
+console.log("JS prendido");
+pintarPJ();
+
+//EVENTOS
+document.addEventListener ("keydown", (ev) =>{
+    if (ev.key === "d" || ev.key === "D" || ev.key === "ArrowRight") {
+        perfilPJ = true;
+        caminar();
+        pintarPJ();
+    } else if (ev.key === "a" || ev.key === "A" || ev.key === "ArrowLeft") {
+        perfilPJ = false;
+        caminar();
+        pintarPJ();
+    } else if (ev.key === "w" || ev.key === "W" || ev.key === "ArrowUp") {
+        jumpPJ = true;
+        caminar();
+        pintarPJ();
+    } 
+});
+
+sun.addEventListener ("click", (ev) =>{
+    bodyAll.classList.add ("sun_body");
+    bodyAll.classList.remove ("moon_body");
+    moon.classList.remove("theme_block");
+    sun.classList.add("theme_block");
+});
+
+moon.addEventListener ("click", (ev) =>{
+    bodyAll.classList.add ("moon_body");
+    bodyAll.classList.remove ("sun_body");
+    moon.classList.add("theme_block");
+    sun.classList.remove("theme_block");
+});
+
+// FUNCION DEL PERSONAJE
+function pintarPJ(ev) {
+    personaje = 
+        `<div class="person" 
+            style="margin-left: ${movePJ}px; margin-bottom: ${skyPJ}px;">
+            <img src="${imgPJ}" alt="imagen de tu personaje">
+        </div>`;
+    mainAll.innerHTML = personaje;
+}
+
+function caminar(){
+     if (perfilPJ === true) {
+        movePJ = movePJ + 10;
+        imgPJ = "avatar-prueba.webp";
+    } else if (perfilPJ === false){
+        movePJ = movePJ - 10;
+        imgPJ = "avatar-prueba_back.webp";
+    } 
+    if (jumpPJ === true){
+        skyPJ = 30;   
+        setTimeout(()=>{
+            skyPJ = 0; 
+            jumpPJ = false;
+            pintarPJ(); 
+        }, 100);
+    }
+}
